a7a234a072c3c541dcb4200499de21d94f249495
Hidestor <30600908+Hidestor@users.noreply.github.com>
2017-11-03 10:57:15 +0530

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,3 +1,7 @@
+#This module is used for converting the dataset into the form which could be directly used in a program
+#in this we are storing the name of the image data cotained in the train dataset or validate dataset into a list and performing
+#the required transformation into the image to extract the image data efficiently and correctly .
+
 import sys
 import os
 import scipy
@@ -10,12 +14,12 @@ from scipy.misc import toimage
 BLUR_AMOUNT = 5
 FINAL_SIZE = 80
 
-TRAIN = 'train/'
-VALIDATE = 'valid/'
-PNG = '.png'
+TRAIN = 'train/' #train directory contains image data used for training the algorithm
+VALIDATE = 'valid/' #valid directory contains image data used for checking validity of the algorithm 
+PNG = '.png'    
 LABELS = 'labels.txt'
 
-DATASET = VALIDATE
+DATASET = VALIDATE #for storing the chosen directory name, initially set to 'valid' direcory
 
 try:
     if sys.argv[1] == 'TRAIN':
@@ -29,22 +33,35 @@ try:
 except:
     DATASET = VALIDATE
 
+#getting the names of all images in the DATASET    
 images = os.listdir(DATASET)
-images.remove(LABELS)
-images = [int(image[:-4]) for image in images]
+
+#deleting the labels.txt file from the directory
+images.remove(LABELS) 
+
+#storing all the file names without extension in a list
+images = [int(image[:-4]) for image in images] 
+
 images.sort()
-images = [str(image) for image in images]
+
+#converting file name from int to string
+images = [str(image) for image in images] 
 
 
 def process(image):
+    
     # apply gaussian filter to image to make text wider
     image = gaussian_filter(image, sigma=BLUR_AMOUNT)
+    
     # invert black and white because most of the image is white
     image = 255 - image
+    
     # resize image to make it smaller
     image = scipy.misc.imresize(arr=image, size=(FINAL_SIZE, FINAL_SIZE))
+    
     # scale down the value of each pixel
     image = image / 255.0
+    
     # flatten the image array to a list
     return [item for sublist in image for item in sublist]
 
